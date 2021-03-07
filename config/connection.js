8276f8e9ff2d0d1eafd7135fa97dba8d9f08318f
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-08 00:10:24 +1100

    adj formatting

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'yourRootPassword',
-    database: 'burgers_db',
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "yourRootPassword",
+  database: "burgers_db",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error(`Error connecting: ${err.stack}`);
-        return;
-    }
-    console.log(`Connected as ID ${connection.threadId}`);
+  if (err) {
+    console.error(`Error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`Connected as ID ${connection.threadId}`);
 });
 
 module.exports = connection;
