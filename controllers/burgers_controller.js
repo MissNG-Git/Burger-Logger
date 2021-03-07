a18de5b5be8856ae4be20d64c007f03b333e79df
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-08 00:05:35 +1100

    add updateOne method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,7 +26,22 @@ router.post("/api/burgers", (req, res) => {
 });
 
 router.put("/api/burgers/:id", (req, res) => {
-  // updateOne()
+  const condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
 });
 
 router.delete("/api/burgers/:id", (req, res) => {
