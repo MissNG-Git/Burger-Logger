9d7fd7737f3b2b1d39a821e34062de60703f98b2
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-08 00:14:00 +1100

    pseudocode events

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,20 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+// CRUD!!!
+
+// UPDATE
+// Set up the event listener for the create button
+// Grabs the id of the element that goes by the name, "id"
+// make sure to serialize the JSON body          // Check that the response is all good
+// Reload the page so the user can see the new quote
+
+// CREATE
+// Grabs the value of the textarea that goes by the name, "quote"
+// Send POST request to create a new quote
+// make sure to serialize the JSON body
+// Empty the form
+// Reload the page so the user can see the new quote
+
+// DELETE
+// Set up the event listeners for each delete button
+// Send the delete request
+// Reload the page
